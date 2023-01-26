096d5d5115961dabb1bf32c3dfc54926ce321d57
sidvas <sid.vas.81@gmail.com>
2023-01-26 16:27:12 +0800

    Use different fetch

--- lib/cryptobot/chat_bot.ex
+++ lib/cryptobot/chat_bot.ex
@@ -2,7 +2,6 @@ defmodule Cryptobot.ChatBot do
 
   def verify(params) do
     chatbot_config = Application.get_env(:cryptobot, :facebook_chat_bot)
-    IO.puts(chatbot_config)
-    params["hub.mode"] == "subscribe" && params["hub.verify_token"] == chatbot_config.webhook_verify_token
+    params["hub.mode"] == "subscribe" && params["hub.verify_token"] == chatbot_config[:webhook_verify_token]
   end
 end
