0d65699f35373eea491ca2f037938f995fc58e70
sidvas <sid.vas.81@gmail.com>
2023-01-26 16:20:50 +0800

    Debugging

--- lib/cryptobot/chat_bot.ex
+++ lib/cryptobot/chat_bot.ex
@@ -2,6 +2,7 @@ defmodule Cryptobot.ChatBot do
 
   def verify(params) do
     chatbot_config = Application.get_env(:cryptobot, :facebook_chat_bot)
+    IO.puts(chatbot_config)
     params["hub.mode"] == "subscribe" && params["hub.verify_token"] == chatbot_config.webhook_verify_token
   end
 end
